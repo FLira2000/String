2dee1b5d5c76036697d84045cba11f4c3c649a7a
Fábio Lira <fabiolira1910@hotmail.com>
2019-10-19 00:40:06 -0300

    forEach implementation
    now, i've created something like a forEach method, that really is a for each loop:
    for each character of the string, do something. It isn't a lambda function properly made, and probaly will not work as mentalized. So, just doing it.

--- String.c
+++ String.c
@@ -5,6 +5,7 @@ typedef struct s{
     char *stuff;
     struct s* self;
     int (*length)(struct s*);
+    void (*forEach)(struct s*, void (*lambda)( void ));
 }String;
 
 int stringLength(String *string){
@@ -13,18 +14,32 @@ int stringLength(String *string){
     return i;
 }
 
+void forEach( String *string, void (*lambda)( void ) ){
+    int i = 0;
+    for(i; string->stuff[i] != '\0'; i++){
+        lambda();
+    }
+}
 
 String* newString( char *string ){
     String *s = (String*) malloc(sizeof(String));
     s->stuff = string;
     s->self = s;
     s->length = &stringLength;
+    s->forEach = &forEach;
     return s;
 }
 
+int callbackCounter = 0;
+
+void callback( void ){
+    printf("callback chamada pela %i vez\n", ++callbackCounter);
+}
 
 int main( void ){
     String *nome = newString("Fabio");
     printf("Nome: %s\n", nome->stuff);
     printf("Tamanho por metodo: %i\n", nome->length(nome->self));
+    nome->forEach(nome->self, callback);
+    callbackCounter = 0;
 }
